cbd96c8398e1fa3eac83052697addad5d4076c7e
RSoffline <fudansi.ryusei@gmail.com>
2016-02-01 21:27:10 +0900

    template matching finish

--- tmpmatch.py
+++ tmpmatch.py
@@ -1,10 +1,23 @@
 import cv2
 import numpy as np
 
-src = cv2.imread("test.jpg")
+src = cv2.imread("cat.jpeg")
 tmp = np.empty((50,50,3), dtype = np.uint8)
 
-h, w, d = src.shape
+srcH, srcW, srcD = src.shape
+tmpH, tmpW, tmpD = tmp.shape
 
-tmp[:,:,:] = src[(h-50)/2:(h+50/2), (w-50)/2:(w+50/2), :]
+tmp[:,:,:] = src[(srcH-tmpH)/2:(srcH+tmpH)/2, (srcW-tmpW)/2:(srcW+tmpW)/2, :]
 
+res = cv2.matchTemplate(src, tmp, cv2.TM_CCOEFF_NORMED)
+
+(minval, maxval, minloc, maxloc) = cv2.minMaxLoc(res)
+
+rect1 = (maxloc[0], maxloc[1])
+rect2 = (maxloc[0] + tmpW, maxloc[1] + tmpH)
+cv2.rectangle(src, rect1, rect2, 0x00ff00)
+
+cv2.imshow("src", src)
+cv2.imshow("tmp", tmp)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
