379017cd59f886d7ef733f43e2a95679bf2c8507
RSoffline <fudansi.ryusei@gmail.com>
2016-02-24 00:09:08 +0900

    add video capture

--- orb_match.py
+++ orb_match.py
@@ -1,13 +1,23 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("cat.jpeg")
-
+src = cv2.imread("cat.jpeg")
+cv2.namedWindow("dst2")
+cap = cv2.VideoCapture(0)
 #detector = cv2.FastFeatureDetector_create()
-detector = cv2.ORB_create()
-keypoints = detector.detect(img)
-dst = cv2.drawKeypoints(img, keypoints, None)
 
+detector = cv2.ORB_create()
+keypoints = detector.detect(src)
+dst = cv2.drawKeypoints(src, keypoints, None)
 cv2.imshow("dst", dst)
-cv2.waitKey(0)
-cv2.destroyWindow("dst")
+
+while True:
+    ret, img = cap.read()
+    keypoints = detector.detect(img)
+    dst = cv2.drawKeypoints(img, keypoints, None)
+    cv2.imshow("dst2", dst)
+    #cv2.imshow("dst", dst)
+    ch = cv2.waitKey(1)
+    if ch == ord("q"):
+        break
+cv2.destroyAllWindow()
